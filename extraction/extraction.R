5441b20d24215553d5bf46874327077fb04e025c
fg <102463666+fgeeri@users.noreply.github.com>
2025-04-07 12:08:31 +0200

    Clean up code

--- extraction/extraction.R
+++ extraction/extraction.R
@@ -57,80 +57,32 @@ for (i in 1:nrow(list_of_cantons)) {
 }
 
 
-list_of_dataframes <- list.files("dataframe_of_laws/")
-
 # List all .rds files in the directory
 rds_files <- list.files(path = "dataframe_of_laws", 
-                        pattern = "*.rds", full.names = TRUE)
+                        pattern = "\\.rds$", full.names = TRUE)
 
-# Read and combine all .rds files into a single dataframe using tidyverse functions
-combined_df <- rds_files %>%
-  map_df(readRDS) %>% 
+combined_df <- map_dfr(rds_files, ~ {
+  df <- readRDS(.x)
+  file_name <- basename(.x)
+  canton <- str_extract(file_name, "(?<=dataframe_of_laws_)[a-z]{2}")
+  language <- str_extract(file_name, "[a-z]{2}(?=\\.rds$)")
+  df %>%
+    mutate(file_name = file_name, canton = canton, language = language)
+}) %>% 
   mutate(url = str_replace(url, "chde/", "ch/app/de/"),
          url = str_replace(url, "chfr/", "ch/app/fr/"),
          url = str_replace(url, "chit/", "ch/app/it/"),
-         url = str_replace(url, "chrm/", "ch/app/rm/"))
+         url = str_replace(url, "chrm/", "ch/app/rm/"),
+         canton = str_to_upper(canton)) %>% 
+  select(abbreviation, url, title, canton, language)
 
-openxlsx::write.xlsx(combined_df, 
-                     file = "combined_df.xlsx")
+# openxlsx::write.xlsx(combined_df, file = "combined_df.xlsx")
 
+zurich <- fromJSON("Zurich/zurich_laws_output.json") %>% 
+  as_tibble()
 
-dol_de <- read_rds("dol_de.rds") %>% 
-  mutate(abbreviation = if_else(law == "110.100", "KV", abbreviation)) %>% 
-  separate_rows(abbreviation, sep = "[,;]") %>% 
-  mutate(abbreviation = str_trim(abbreviation),
-         language = "de")
-
-dol_rm <- read_rds("dol_rm.rds") %>% 
-  separate_rows(abbreviation, sep = "[,;]") %>% 
-  mutate(abbreviation = str_trim(abbreviation),
-         language = "rm") %>% 
-  mutate(abbreviation = if_else(abbreviation != "", 
-                                paste(language, abbreviation), NA))
-
-dol_it <- read_rds("dol_it.rds") %>% 
-  separate_rows(abbreviation, sep = "[,;]") %>% 
-  mutate(abbreviation = str_trim(abbreviation),
-         language = "it") %>% 
-  mutate(abbreviation = if_else(abbreviation != "", 
-                                paste(language, abbreviation), NA))
-
-dol_de2 <- dol_de %>%
-  bind_rows(  # One-word titles become abbreviations
-    dol_de %>%
-      filter(str_count(title, "\\s") == 0) %>%
-      mutate(abbreviation = title)
-  ) %>%
-  bind_rows(  # Two-word "Kantonales?" titles also become abbreviations
-    dol_de %>%
-      filter(str_detect(title, "^Kantonales?\\s[A-Za-zäöü]+$")) %>%
-      mutate(abbreviation = str_remove(title, "Kantonales? "))  # Two-word titles become abbreviations
-  ) %>%
-  bind_rows(  # Two-word "Kantonales?" titles also become abbreviations without "kantonales"
-    dol_de %>%
-      filter(str_detect(title, "^Kantonales?\\s[A-Za-zäöü]+$")) %>%
-      mutate(abbreviation = title)  # Keep full abbreviation
-  ) %>%
-  bind_rows(  # Two-word "Kantonales?" abbreviations also become abbreviations without "kantonales"
-    dol_de %>%
-      filter(str_detect(abbreviation, "^Kantonales?\\s[A-Za-zäöü]+$")) %>%
-      mutate(abbreviation = str_remove(abbreviation, "Kantonales? "))
-  )
-
-dol3 <- bind_rows(dol_de2, dol_rm, dol_it) %>% 
-  arrange(law) %>% 
-  mutate(abbreviation = na_if(abbreviation, ""),  # Remove empty strings
-         abbreviation_lower = str_to_lower(abbreviation),  # Turn into lowercase
-         url = paste0("https://www.gr-lex.gr.ch/app/", language, "/texts_of_law/", law)) %>%  # Create URLs
-  filter(is.na(abbreviation) == FALSE)
-
-dol3 %>% 
-  select(abbreviation_lower, url) %>% 
-  bind_rows(tibble(abbreviation_lower = c("zgb", "lol"),
-                   url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
-            tibble(abbreviation_lower = c("plattner", "pla"),
-                   url = "https://doi.org/10.38107/021"),
-            tibble(abbreviation_lower = c("iktv"),
-                   url = "https://www.gr-lex.gr.ch/app/de/texts_of_law/170.500")) %>% 
+bind_rows(combined_df,
+          zurich) %>% 
   toJSON(pretty = TRUE) %>% 
   write(file = "abbreviations.json")
+
